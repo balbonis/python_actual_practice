0469edd1a230004c858371180cf8b1c8f97da1db
balbonis <leyritana.jun.se@icloud.com>
2026-03-04 19:45:15 +0800

    Implement palindrome check in palindrome.py
    Fix typo in replace method and add input prompt.

---
+++ palindrome.py
@@ -0,0 +1,7 @@
+text = input ("Enter Text: ")
+text = text.repalce(" ","")
+
+if text.lower() == text[::-1].lower():
+  print(f"{text} This is a palindrome.")
+else:
+  print(f"{text} This is not palindrome.")
