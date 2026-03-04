a9ff5a7d2750c7e74a1c4eba5aab7a0194c3d51e
balbonis <leyritana.jun.se@icloud.com>
2026-03-05 01:14:16 +0800

    Update input prompt and print range of numbers
    Refactor input prompt and add range printing for max number.

--- list_max_number.py
+++ list_max_number.py
@@ -2,12 +2,11 @@
 # Example:
 # Input: "3 5 7 2 8"
 # Output: "The max number is 8" 
-
-numbers = [int (n) for n in input ("Enter Numbers sep by space: ").split()]
+numbers = [int(n) for n in input("Enter # sep by space:").split()]
 print (f"The max number is: ", max(numbers))
 
+# Range function to print numbers from 1 to the maximum number found in the list. It uses the built-in max() function to determine the maximum value and then iterates through a range starting from 1 up to and including that maximum number, printing each number in the range.
+my_range = max(numbers)
+for ctr in range(1, my_range+1):
+    print (ctr)
 
-#List the number from 1 to max_numbe automatically
-my_range = int(max_number) 
-for n in range (1, my_range+1):
-    print(n)    
