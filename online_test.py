009e0b44c855d41b86b68256f8edf8fdb12a9385
balbonis <leyritana.jun.se@icloud.com>
2026-03-05 06:10:51 +0800

    Add continuous input loop for various tests
    Implemented a continuous loop for user input to find max number, check for palindromes, and count word frequencies. Added exception handling for graceful exit.

---
+++ online_test.py
@@ -0,0 +1,39 @@
+try:
+
+    while True:
+        
+        #max number and range
+        numbers = [int(n) for n in input ("Enter numbers:").split()]
+        print (f"Max number is ", max(numbers))
+        for ctr in range(1,max(numbers)+1):
+            print (f"count {ctr}")
+        
+        
+        
+        #palindrome
+        text = input("Enter word:").replace(' ','').lower()
+        if text==text[::-1]:
+            print ("Palindrome")
+        else:
+            print ("Not Palindrome")
+            
+            
+            
+        #word frequency
+        from collections import Counter
+        text = input("Enter words:")
+        counts = Counter(text.split())
+        
+        print (counts)
+        print (list(counts.keys())," ",list(counts.values()))
+        
+        
+        
+        
+        
+            
+            
+        
+except KeyboardInterrupt:
+    
+    print("App closed")
